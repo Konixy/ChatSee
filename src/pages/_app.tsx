58875e66010e3d0b4c3a0c36aec79f234a7129e9
Anatole Dufour <konixy.p@gmail.com>
2023-01-24 11:58:59 +0100

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: { Component: AppComponent;
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Chat See</title>
       </Head>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function _document() {
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/images/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Web site created using create-react-app" />
         <link rel="apple-touch-icon" href="/images/logo150.png" />
