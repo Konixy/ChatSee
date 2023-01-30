bcd8c139ab77428d5233860add6874d4c6f95f41
Konixy <konixy.p@gmail.com>
2023-01-30 21:32:08 +0100

    Update

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import * as IronSession from 'iron-session';
-import { APIUser } from 'lib/types';
-
-declare module 'iron-session' {
-  interface IronSessionData {
-    user?: APIUser;
-  }
-}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   req.statusCode = 200;
--- pages/api/user/auth.ts
+++ pages/api/user/auth.ts
@@ -1,15 +1,9 @@
 import database from 'lib/database';
-import cookies from 'lib/cookies';
+import withSession from 'lib/session';
 
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//   res.send('test');
-// }
-
-import { withIronSessionApiRoute } from 'iron-session/next';
-
-export default withIronSessionApiRoute(async (req, res) => {
+export default withSession(async (req, res) => {
   if (!req.body) return res.send({ success: false, message: 'invalid request body' });
 
   await req.session.save();
   res.send({ ok: true });
-}, cookies);
+});
